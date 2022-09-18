41303bfb82100e671094d7c655c7eb9cb2da9b04
Carlos Gómez García <cgomez.egacia@gmail.com>
2022-09-18 18:35:07 -0500

    continuacion de la leccion sobre entry

--- tkinter/entry.py
+++ tkinter/entry.py
@@ -7,9 +7,21 @@ ventana.title('Manejo de entry')
 ventana.iconbitmap('icono.ico')
 
 # width cantidad de caracteres a ocupar en la caja
-entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER)
+
+# show: para indiciar que componente se va mostrar independientemente
+# de lo que escriba el usuario. Pero lo que escriba el usuario si se podra
+# recuperar correctamente, es util por ejemplo cuando se escribe una contraseña 
+# entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER, show='*')
+
+# state=tk.DISABLED: desabilita el componente. Si state tiene NORMAL
+# se podra volver a caputar el texto con normalidad
+entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER, state=tk.NORMAL)
 entrada1.grid(row=0, column=0)
 # insert agrefa un texto a la caja
 entrada1.insert(0,'Introduce una cadena')
+entrada1.insert(tk.END, '.')
+
+# state='readonly' se configurar como solo lectura y no se podra modificar
+entrada1.config(state='readonly')
 
 ventana.mainloop()
